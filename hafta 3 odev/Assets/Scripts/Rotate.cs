eb50c0db2b965995065f45908050d3e8666fc41a
Hüseyin Uzun <uzunhuseyin1967@gmail.com>
2023-11-29 17:00:26 +0300

    Mobil Oyun Akademisi Ödevi
    Ödev Ekleme işlemi

---
+++ hafta 3 odev/Assets/Scripts/Rotate.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    public Vector3 vect3;
+    void Start()
+    {
+    
+        vect3 = new Vector3(0,0.2f,0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(vect3);
+    }
+}
